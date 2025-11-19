b9e31b3839e9c3046def1caaeddab97d7b9631c1
rivoten <rivoten05@gmail.com>
2025-11-19 11:49:01 +0530

    nvim config

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,23 @@
+return {
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = function()
+			require("toggleterm").setup({
+				shell = "nu",
+				start_in_insert = true,
+				insert_mappings = true,
+				close_on_exit = true,
+				direction = "float",
+				persist_size = true,
+				persist_mode = true,
+				size = 20,
+				open_mapping = [[<c-\>]],
+				hide_numbers = true,
+				float_opts = {
+					border = "rounded",
+				},
+			})
+		end,
+	},
+}
