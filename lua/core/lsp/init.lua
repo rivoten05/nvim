18eaf5fd1957c620805adb1c630cd54fa0310944
rivoten <rivoten05@gmail.com>
2025-11-19 18:03:36 +0530

    Add: virtual lines

--- lua/core/lsp/init.lua
+++ lua/core/lsp/init.lua
@@ -9,31 +9,6 @@ function M.setup()
 	for _, lsp in ipairs(servers) do
 		vim.lsp.enable(lsp)
 	end
-
-	vim.diagnostic.config({
-		virtual_lines = true,
-
-		-- virtual_text = true,
-		underline = true,
-		update_in_insert = false,
-		severity_sort = true,
-		float = {
-			border = "rounded",
-			source = true,
-		},
-		signs = {
-			text = {
-				[vim.diagnostic.severity.ERROR] = "󰅚 ",
-				[vim.diagnostic.severity.WARN] = "󰀪 ",
-				[vim.diagnostic.severity.INFO] = "󰋽 ",
-				[vim.diagnostic.severity.HINT] = "󰌶 ",
-			},
-			numhl = {
-				[vim.diagnostic.severity.ERROR] = "ErrorMsg",
-				[vim.diagnostic.severity.WARN] = "WarningMsg",
-			},
-		},
-	})
 end
 
 return M
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -40,6 +40,29 @@ return {
 		event = "LspAttach",
 		config = function()
 			require("lsp_lines").setup()
+			vim.diagnostic.config({
+				virtual_lines = true,
+				virtual_text = false,
+				underline = true,
+				update_in_insert = false,
+				severity_sort = true,
+				float = {
+					border = "rounded",
+					source = true,
+				},
+				signs = {
+					text = {
+						[vim.diagnostic.severity.ERROR] = "󰅚 ",
+						[vim.diagnostic.severity.WARN] = "󰀪 ",
+						[vim.diagnostic.severity.INFO] = "󰋽 ",
+						[vim.diagnostic.severity.HINT] = "󰌶 ",
+					},
+					numhl = {
+						[vim.diagnostic.severity.ERROR] = "ErrorMsg",
+						[vim.diagnostic.severity.WARN] = "WarningMsg",
+					},
+				},
+			})
 		end,
 	},
 }
