aaf272c900a4f0ae71f999c6d5cc6f0a454a469e
rivoten <rivoten05@gmail.com>
2025-11-20 13:55:43 +0530

    improve dressing

--- lua/plugins/dressing.lua
+++ lua/plugins/dressing.lua
@@ -1,33 +1,4 @@
 return {
 	"stevearc/dressing.nvim",
-	event = "VeryLazy",
 	opts = {},
-	config = function()
-		require("dressing").setup({
-			input = {
-				backend = { "nui", "builtin" }, -- Fallback to builtin if nui isn't available
-				nui = {
-					border = {
-						style = "rounded",
-						text = { top = "[Input]", top_align = "left" },
-					},
-					relative = "editor",
-					position = "50%",
-					size = { width = "50%", height = 1 },
-				},
-			},
-			select = {
-				backend = { "nui", "builtin" },
-				nui = {
-					border = {
-						style = "rounded",
-						text = { top = "[Select]", top_align = "left" },
-					},
-					relative = "editor",
-					position = "50%",
-					size = { width = "50%", height = "50%" },
-				},
-			},
-		})
-	end,
 }
