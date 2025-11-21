ac13e1440acb3c267486961d2bc40834f728bf72
rivoten <rivoten05@gmail.com>
2025-11-21 12:07:10 +0530

    theme switch add

--- lua/configs/keymaps-ui.lua
+++ lua/configs/keymaps-ui.lua
@@ -7,13 +7,12 @@ basic_map("n", "<leader>h", "<cmd>Alpha<CR>", "Alpha")
 -- Reload nvim (LINE 8/9 area)
 basic_map("n", "<leader>ur", "<Cmd>update<CR><Cmd>source<CR>", "Reload nvim")
 
-vim.keymap.set("n", "<leader>ut", function()
+safe_map("n", "<leader>ut", function()
 	require("telescope.builtin").colorscheme({
 		enable_preview = true,
 	})
-end, { desc = "Telescope: Colorscheme Picker (Live Preview)" })
+end, "Telescope: Colorscheme Picker (Live Preview)")
 
--- Show Lazy logs
 basic_map("n", "<leader>pl", "<cmd>Lazy log<CR>", "Show Lazy logs")
 
 -- Check plugin health
